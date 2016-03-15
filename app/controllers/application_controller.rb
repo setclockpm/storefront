4aab4bb92950dbff58c9403ef08a475df6c7eead
Phillip Medrano <phillip.medrano@gmail.com>
2016-03-15 02:30:09 -0700

    added front end pkg managers and bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
-  include ShopifyApp::Controller
+  #include ShopifyApp::Controller
+  before_action :authenticate_user!
   protect_from_forgery
 end
