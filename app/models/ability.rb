d7991c08633bcb45a2b4c4880bcc8bc7d02e6817
Phillip Medrano <phillip.medrano@gmail.com>
2016-03-15 03:06:34 -0700

    added can can block

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,12 +4,14 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
+    def initialize(user)
+      user ||= User.new # guest user
+
+      if user.role? :admin
+        can :manage, :all
+      end
+        can :read, :all
+      end  
     #
     # The first argument to `can` is the action you are giving the user
     # permission to do.
