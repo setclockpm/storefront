e0d83497904a3dc180ffe4454218c177822198b5
Phillip Medrano <phillip.medrano@gmail.com>
2016-03-18 20:28:18 -0700

    add admin to user

--- config/application.rb
+++ config/application.rb
@@ -47,7 +47,7 @@ module Porthos
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
-    # config.active_record.schema_format = :sql
+    config.active_record.schema_format = :sql
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
