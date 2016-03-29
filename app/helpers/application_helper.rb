de6fb04ef580d5d97337a803cc1b3ee172bcb736
Phillip Medrano <phillip.medrano@gmail.com>
2016-03-29 14:41:54 -0700

    title for customer facing pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
 
   def title(page_title)
+    page_title = "Titty Sprinkles"
     content_for(:title) { page_title }
   end
 end
--- app/overrides/admin_title.rb
+++ app/overrides/admin_title.rb
@@ -1,7 +1,8 @@
-Deface::Override.new(virtual_path:     'spree/admin/shared/_head',
-                     name:             'override',
-                     replace_contents: 'title',
-                     text:             'Porthos Portal - <%= controller.controller_name.titleize %>')
+Deface::Override.new(name:             'override-admin-title',
+                     virtual_path:     'spree/admin/shared/_head',
+                     replace_contents: 'title', text: 'Porthos Portal - <%= controller.controller_name.titleize %>')
 
 
-                     
+Deface::Override.new(name:             'override-storefront-title',
+                     virtual_path:     'spree/shared/_head',
+                     replace_contents: 'title', text: 'Porthos Home - <%= title = (controller.controller_name.titleize || "Modern, Elegant, Simple") %>')
