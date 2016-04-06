0b61d071af1fd68a54a400be5d9d0376acc7d320
Phillip Medrano <phillip.medrano@gmail.com>
2016-04-06 00:48:27 -0700

    under construction

--- app/overrides/landing.rb
+++ app/overrides/landing.rb
@@ -1,8 +1,16 @@
-Deface::Override.new(virtual_path: 'spree/home/index',
-            replace: '[data-hook="homepage_products"]',
-            name: 'landing-products',
-            partial: 'welcome/hello')
+Deface::Override.new(name:         'landing-products',
+                     virtual_path: 'spree/home/index',
+                     replace:      '[data-hook="homepage_products"]',
+                     partial:      'welcome/hello')
 
-    Deface::Override.new(virtual_path: 'spree/home/index',
-            name: 'landing-sidebar',
-            remove: '[data-hook="homepage_sidebar_navigation"]')
+
+Deface::Override.new(name: 'landing-sidebar',
+                     virtual_path: 'spree/home/index',
+                     remove: '[data-hook="homepage_sidebar_navigation"]')
+
+
+Deface::Override.new(name:         'under-construction',
+                     virtual_path: 'spree/layouts/spree_application',
+                     replace:      'div#content',
+                     partial:      'welcome/hello')
+            
