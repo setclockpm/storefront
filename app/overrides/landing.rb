b24453334c118e56d24b5c166b1385aa2a137f13
Phillip Medrano <phillip.medrano@gmail.com>
2016-04-07 02:13:14 -0700

    landing page 1 and template uploaded

--- app/overrides/landing.rb
+++ app/overrides/landing.rb
@@ -1,16 +1,23 @@
-# Deface::Override.new(name:           'landing-products',
-#                      virtual_path:   'spree/home/index',
-#                      replace:        '[data-hook="homepage_products"]',
-#                      partial:        'welcome/hello')
+Deface::Override.new(name:           'landing-products',
+                     virtual_path:   'spree/home/index',
+                     replace:        '[data-hook="homepage_products"]',
+                     partial:        'welcome/hello')
 
 
 Deface::Override.new(name:             'landing-sidebar',
                      virtual_path:     'spree/home/index',
-                     remove:           '[data-hook="homepage_sidebar_navigation"]')
+                     remove:           '[data-hook="homepage_sidebar_navigation"]',
+                     original:         'd9a21ccc62b9787b5b2f24d6baf226dba776e4a3')
 
 
 Deface::Override.new(name:             'main-landing-page',
                      virtual_path:     'spree/layouts/spree_application',
-                     replace_contents: 'div.container',
-                     partial:          'welcome/hello')
+                     replace_contents: '[data-hook="body"]',
+                     partial:          'welcome/landing_main')
+                     
+                     
+Deface::Override.new(name:             'replace-header-with-nav',
+                     virtual_path:     'spree/shared/_header',
+                     replace_contents: 'div#spree-header',
+                     partial:          'layouts/navigation')
             
