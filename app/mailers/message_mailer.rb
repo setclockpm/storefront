27cda1e495914706fe0f04953dbb0137e35686dd
Phillip Medrano <phillip.medrano@gmail.com>
2016-06-20 16:30:07 -0700

    template for email

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -9,8 +9,8 @@ class MessageMailer < ActionMailer::Base
     mail subject: "Message from #{message.name}", from: "worker@porthoshomeapplication.com", reply_to: reply_to_address, cc: "phsia@porthoshome.com"
   end
   
-  def inventory_sync_report(skus_to_look_into)
-    @skus = skus_to_look_into
+  def inventory_sync_report(variants_needing_attn)
+    @variants = variants_needing_attn
     reply_to_address = "Porthos Server (Do Not Reply) \<donotreply@porthoshome.com\>"
     
     mail subject: "SKU Possibly Requiring Attention", from: "worker@porthoshomeapplication.com", reply_to: reply_to_address, to: "phsia87@gmail.com"
