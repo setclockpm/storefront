432767082b5bcd79677059b793ef03f4901fa6d4
Phillip Medrano <phillip.medrano@gmail.com>
2016-07-23 02:50:41 -0700

    styled properties

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -14,14 +14,6 @@ module ProductsHelper
     color_options(v).downcase unless v.images.any?
   end
   
-  def variant_primary_thumbnail_url(v)
-    if v.primary_thumbnail_url
-      image_tag(v.primary_thumbnail_url, class: "thumbnail") 
-    else
-      image_tag 'no-image.png', class: "no-thumbnail thumbnail", size: '56x56'
-    end
-  end
-  
   def lifestyle_background_image(product)
     product.lifestyle_shot? ? image_tag(variant.images.first.attachment.url(:product)) : "ls-placeholder.jpg"
   end
@@ -31,6 +23,23 @@ module ProductsHelper
   end
   def prev_page_button(on_first_pg, is_remote, url)
     on_first_pg ? page_decoy("previous") : prev_page_link(is_remote, url)
+  end
+  
+  
+  def specifications
+    # Remove properties that are ambiguous 'Features' (to be used in another section)
+    @features           = @product_properties.select{|pp| pp.property.name =~ /features/i }
+    @product_properties = @product_properties - @features
+    # Remove properties with blank values
+    @product_properties.reject{|s| s.value.blank? }
+  end
+  
+  def variant_primary_thumbnail_url(v)
+    if v.primary_thumbnail_url
+      image_tag(v.primary_thumbnail_url, class: "thumbnail") 
+    else
+      image_tag 'no-image.png', class: "no-thumbnail thumbnail", size: '56x56'
+    end
   end
   
   
--- app/overrides/products.rb
+++ app/overrides/products.rb
@@ -11,14 +11,6 @@ Deface::Override.new(name:             'expose-color-option-radio-buttons',
                      partial:          'orders/cart',
                      original:         nil)
 
-
-Deface::Override.new(name:             'add-flex-to-product-page',
-                     virtual_path:     'spree/products/show',
-                     insert_after:     'div[data-hook="product_show"]',
-                     partial:          'products/flex_image',
-                     original:         'd70e5231de1ae54406e3f9b732b12679cfcb1374')
-
-
 Deface::Override.new(name:             'add-xs-to-product-right-part',
                      virtual_path:     'spree/products/show',
                      set_attributes:   'div[data-hook="product_right_part"]',
@@ -27,27 +19,37 @@ Deface::Override.new(name:             'add-xs-to-product-right-part',
 
 #######################################################################################
 ### The next few overrides handle re-arranging product attributes in cart-form ########
-Deface::Override.new(name:             'place-product-desc',
-                     virtual_path:     'spree/products/show',
-                     insert_after:     'div[data-hook="lifestyle_main"]',
-                     cut:              'div[data-hook="description"]',
-                     original:         nil, disabled: false)
-                     
-Deface::Override.new(name:             'format-product-desc',
-                     virtual_path:     'spree/products/show',
-                     replace_contents: 'div[data-hook="description"]',
-                     text:             '<p class="description-block"><%= product_description(@product) %></p>',
-                     original:         nil)
-
 Deface::Override.new(name:             'add-centered-to-product-desc',
                      virtual_path:     'spree/products/show',
                      set_attributes:   'div[data-hook="description"]',
                      attributes:       { class: 'well text-center' },
                      original:         nil)
                      
+Deface::Override.new(name:             'move-product-properties',
+                     virtual_path:     'spree/products/show',
+                     remove:           'div[data-hook="product_properties"]',
+                     original:         nil, disabled: false)
+
+Deface::Override.new(name:             'format-product-desc',
+                     virtual_path:     'spree/products/show',
+                     replace_contents: 'div[data-hook="description"]',
+                     text:             '<p class="description-block"><%= product_description(@product) %></p>',
+                     original:         nil)
+                     
+Deface::Override.new(name:             'add-flex-to-product-page',
+                     virtual_path:     'spree/products/show',
+                     insert_after:     'div[data-hook="product_show"]',
+                     partial:          'products/flex_image',
+                     original:         'd70e5231de1ae54406e3f9b732b12679cfcb1374')
+                     
+Deface::Override.new(name:             'place-product-desc',
+                     virtual_path:     'spree/products/show',
+                     insert_after:     'div[data-hook="lifestyle_main"]',
+                     cut:              'div[data-hook="description"]',
+                     original:         nil, disabled: false)
+                     
 Deface::Override.new(name:             'place-taxon-options',
                      virtual_path:     'spree/products/show',
                      insert_after:     'div[data-hook="description"]',
                      cut:              'div[data-hook="product_taxons"]',
                      original:         nil, disabled: false)
-
