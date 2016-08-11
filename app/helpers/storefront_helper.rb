9b054163fafa5eb871a1a50c9363ddd4fb97f95f
Phillip Medrano <phillip.medrano@gmail.com>
2016-08-10 23:38:46 -0700

    front page borders / colors

--- app/helpers/storefront_helper.rb
+++ app/helpers/storefront_helper.rb
@@ -21,7 +21,7 @@ module StorefrontHelper
   end
   
   def link_to_catalog(text="Download Catalog")
-    link_to fa_icon("download", text: text), main_app.download_catalog_url, class: "btn btn-lg btn-outline"
+    link_to text.upcase, main_app.download_catalog_url, class: "btn btn-lg btn-outline"
   end
   
   def social_media_icons
