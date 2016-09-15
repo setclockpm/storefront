52ea5c029fb3f2dea7f1ab189c73a47c41bd80ab
Phillip Medrano <phillip.medrano@gmail.com>
2016-09-15 16:36:02 -0700

    syntax fix

--- app/helpers/previews_helper.rb
+++ app/helpers/previews_helper.rb
@@ -5,21 +5,21 @@ module PreviewsHelper
   end
   
   def preview_thumbnail(item)
-    if item.default_image?
-      image_tag(item.default_image, alt: item.name)
-    else
-      image_tag("missing_preview.png", alt: "No image for this creation")
-    end
+    # if item.default_image?
+#       image_tag(item.default_image, alt: item.name)
+#     else
+#       image_tag("missing_preview.png", alt: "No image for this creation")
+#     end
   end
   
   def preview_reference(creation)
-    padded_revision = 'r%04d' % creation.revision
-    link = link_to(creation_thumbnail(creation),
-                   creation,
-                   title: "More info on #{creation.name}")
-    size = number_to_human_size(creation.filesize)
-    raw("#{link} (#{size}, #{creation.stage},
-        #{padded_revision}, #{creation.format.upcase})")
+    # padded_revision = 'r%04d' % creation.revision
+  #   link = link_to(creation_thumbnail(creation),
+  #                  creation,
+  #                  title: "More info on #{creation.name}")
+  #   size = number_to_human_size(creation.filesize)
+  #   raw("#{link} (#{size}, #{creation.stage},
+  #       #{padded_revision}, #{creation.format.upcase})")
   end
   
 end
