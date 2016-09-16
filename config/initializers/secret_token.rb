1cf1af6ca6155dacab4ce9c5cb7d49b9d6099ada
Phillip Medrano <phillip.medrano@gmail.com>
2016-09-16 14:56:16 -0700

    this was never needed / never was a rails 3.x app

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Porthos::Application.config.secret_token = '4efc888388a601f484702bf1ca26db3dad4ec8bc0ec609fd1f42a833677eb339dbbf4cdb4829a0409e2a80b2d46867fc5d34b937b10d364adf4600b1b7a0eaf5'
