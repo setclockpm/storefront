f43e4f3999047e9fa3c24e8842ba4d2ba47273cb
Phillip Medrano <phillip.medrano@gmail.com>
2016-09-22 03:05:26 -0700

    organized photos by section / finished coll preview

--- app/helpers/previews_helper.rb
+++ app/helpers/previews_helper.rb
@@ -1,26 +1,26 @@
 module PreviewsHelper
   
   def collection_preview_items
-    ["preview1.png", "preview2.png", "preview3.png"]
+   { "preview0.jpg" => { "on Wayfair" => 'http://www.wayfair.com/Porthos-Home-B40284.html' }, 
+     "preview1.jpg" => { "in our gallery" => collection_path }, 
+     "preview2.jpg" => { "on Overstock" => 'http://www.overstock.com/Porthos-Home,/brand,/results.html' }
+   }
   end
   
-  def preview_thumbnail(item)
-    # if item.default_image?
-#       image_tag(item.default_image, alt: item.name)
-#     else
-#       image_tag("missing_preview.png", alt: "No image for this creation")
-#     end
+  def preview_reference(image, dest)
+    content_tag :figure do
+      preview_thumbnail(image).concat(preview_caption(dest))
+    end
   end
   
-  def preview_reference(image, index)
-    
-    # padded_revision = 'r%04d' % creation.revision
-  #   link = link_to(creation_thumbnail(creation),
-  #                  creation,
-  #                  title: "More info on #{creation.name}")
-  #   size = number_to_human_size(creation.filesize)
-  #   raw("#{link} (#{size}, #{creation.stage},
-  #       #{padded_revision}, #{creation.format.upcase})")
-  end
+  
+  private
+    def preview_caption(dest)
+      content_tag :figcaption, (content_tag :h3, "View collection #{dest}")
+    end
+  
+    def preview_thumbnail(img)
+      image_tag "preview/#{img}", title: "furniture collection preview", class: "collection-preview-image"
+    end
   
 end
