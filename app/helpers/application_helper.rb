7c4c3469df3f80b996a73df8493f7ef34f118539
Phillip Medrano <phillip.medrano@gmail.com>
2016-09-23 11:37:01 -0700

    currently at helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -32,6 +32,16 @@ module ApplicationHelper
     end
   end
   
+  def nav_tab(title, url, options={})
+    current_tab = options.delete(:current)
+    options[:class] = (current_tab == title) ? 'active nav-item' : 'inactive nav-item'
+    content_tag(:li, link_to(title, url), options)
+  end
+
+  def currently_at(tab)
+    render partial: 'layouts/shared/main_nav', locals: { current_tab: tab }
+  end
+  
   def required_field
     content_tag :span, '*', class: 'foo'
   end
