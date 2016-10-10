6415729c8c149917ac5a7cd32fce47bfa9151ee3
Phillip Medrano <phillip.medrano@gmail.com>
2016-10-09 17:43:33 -0700

    using rbenvpvars plugin for env variables

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :repo_url, 'git@github.com:porthoshome/storefront.git'
 # set :pty, true
 
 # Default value for :linked_files is []
- set :linked_files, %w{config/database.yml}
+ set :linked_files, %w{config/database.yml, .rbenv-vars}
 
 # Default value for linked_dirs is []
  set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
