8a5353308d28424d675357fd8ab40e4021a3947c
Phillip Medrano <phillip.medrano@gmail.com>
2016-12-10 03:54:36 -0800

    showcase validation streamlined/refactored

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -2,8 +2,7 @@
 Spree::Product.class_eval do
   ALLOWED_SHOWCASE_ITEMS = 3 unless const_defined?(:ALLOWED_SHOWCASE_ITEMS)
 
-  validate :showcased_items_count, on: :create
-  validate :other_showcased_items_count, on: :update
+  validate :max_showcased_items_count_not_exceeded
   
   # Class methods
   def self.showcased_items
@@ -23,22 +22,16 @@ Spree::Product.class_eval do
   
   
   private
-    def other_showcased_items_count
-      unless showcased_items_count_valid?(id)
-        errors.add(:base, "Only up to #{ALLOWED_SHOWCASE_ITEMS} showcased products are allowed.", count: ALLOWED_SHOWCASE_ITEMS)
-      end
-    end
     
-    def showcased_items_count
-      unless showcased_items_count_valid?
-        errors.add(:base, "Only up to #{ALLOWED_SHOWCASE_ITEMS} showcased products are allowed.", count: ALLOWED_SHOWCASE_ITEMS)
-      end
-    end
-    
-    def showcased_items_count_valid?(reject_id=-1)
-      showcased_items = self.class.showcased_items.reject{|si| si.id == reject_id }
-      puts "Showcased items: #{showcased_items.map{|s| [s.id, s.name] }} \n\n\n #{showcased_items.size}\n\n\n"
-      showcased_items.size <= ALLOWED_SHOWCASE_ITEMS
+    def max_showcased_items_count_not_exceeded
+      current_showcased_items = self.class.showcased_items
+      return if current_showcased_items.size < ALLOWED_SHOWCASE_ITEMS
+      # We're at max
+      return if current_showcased_items.map(&:id).include?(id)
+      # We don't count if this record is one of the showcased. (Can never increase showcase count)
+      return unless attributes['showcased']
+      # We don't ccre unless another record trying to becime showcased
+      errors.add(:base, :too_many_showcased_items, count: ALLOWED_SHOWCASE_ITEMS)
     end
   
 end
