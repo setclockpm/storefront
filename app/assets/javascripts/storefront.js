33e298e0f6ecca121eedc1ed07ee47adc3b2d522
Phillip Medrano <phillip.medrano@gmail.com>
2016-12-21 23:28:20 -0800

    catalog now a partial

--- app/assets/javascripts/storefront.js
+++ app/assets/javascripts/storefront.js
@@ -71,7 +71,7 @@ $(document).ready(function(){
       enabled: true, // By default it's false, so don't forget to enable it
       // The "opener" function should return the element from which popup will be zoomed in
       // and to which popup will be scaled down
-      // By defailt it looks for an image tag:
+      // By default it looks for an image tag:
       opener: function(openerElement) {
         // openerElement is the element on which popup was initialized, in this case its <a> tag
         // you don't need to add "opener" option if this code matches your needs, it's defailt one.'
