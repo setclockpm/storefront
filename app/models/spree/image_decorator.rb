04e5a1baae427f70e5e0a94c9bfed32721776a79
Phillip Medrano <phillip.medrano@gmail.com>
2016-12-30 03:16:28 -0800

    restored orignal small dimensions

--- app/models/spree/image_decorator.rb
+++ app/models/spree/image_decorator.rb
@@ -6,7 +6,7 @@ Spree::Image.class_eval do
                     default_style:   :product,
                     path:            'public/inventory/:master_sku/:style-:s3_slug-:id.:image_file_extension',
                     storage:         :s3,
-                    styles:          { mini: '48x48>', small: '120x80>', preview: '480x360>', product: '1024x768>', large: '2048x1536>', lightbox: '800x600>' },
+                    styles:          { mini: '48x48>', small: '240x160>', preview: '480x360>', product: '1024x768>', large: '2048x1536>', lightbox: '800x600>' },
                     s3_credentials:  { access_key_id: Rails.application.secrets.aws_access_key_id, secret_access_key: Rails.application.secrets.aws_secret_access_key },
                     s3_protocol:     "https",
                     s3_region:       'ap-southeast-1',
